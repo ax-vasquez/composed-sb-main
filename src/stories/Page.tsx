c20f6ca1aff98f639d29767e76104fde1525ec6c
Armando Vasquez <mando@mcnuggies.dev>
2025-05-08 15:34:32 -0500

    link to donor SB

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
 
-import { Header } from './Header';
 import './page.css';
 
-type User = {
-  name: string;
-};
 
 export const Page: React.FC = () => {
-  const [user, setUser] = React.useState<User>();
 
   return (
     <article>
-      <Header
-        user={user}
-        onLogin={() => setUser({ name: 'Jane Doe' })}
-        onLogout={() => setUser(undefined)}
-        onCreateAccount={() => setUser({ name: 'Jane Doe' })}
-      />
 
       <section className="storybook-page">
         <h2>Pages in Storybook</h2>
@@ -36,7 +25,7 @@ export const Page: React.FC = () => {
         <ul>
           <li>
             Use a higher-level connected component. Storybook helps you compose such data from the
-            "args" of child component stories
+            &quot;args&quot; of child component stories
           </li>
           <li>
             Assemble data in the page component from your services. You can mock these services out
